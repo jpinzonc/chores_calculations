1685bde71a3f07c57304ca42dc1b418d3e9daa9d
jpinzonc <jpinzon@hotmail.com>
2018-09-12 18:10:57 -0500

    CODE: Finished preliminary functions

--- Chores.R
+++ Chores.R
@@ -1,43 +1,42 @@
 # Loading required libraries
+
 library(googlesheets)
 suppressMessages(library(dplyr))
-
 # Check the list of sheets
 my_sheets <- gs_ls()
 # Listing the table of interest:
-gs_ls("NEW House Chores")
+gs_ls("NEW")
 # Getting the data from the sheet
 # reading the file
-gap <- gs_title("NEW House Chores")
+gap <- gs_title("NEW")
 # Checking the sheets in the spreadsheet
 gs_ws_ls(gap)
 # Getting the data from the responses
 data = gap %>% gs_read(ws = "Form Responses 1")
+total_rows = nrow(data)
 # generating a date field.
 data$Day = as.Date(data$Timestamp,"%m/%d/%Y")
-# Adding the total number of point for each child. 
-data%>%group_by(Name)%>%summarise(t_points = sum(Points))
-data
-#####
+# NON PAID DATA
+ndata = data%>%filter(is.na(paid))
+n_rows = nrow(ndata)
+if (total_rows == n_rows){n_rows = n_rows-1}
 
-data %>% 
-  select_if(function(x) any(is.na(x))) %>% 
-  summarise_all(funs(sum(is.na(.)))) -> f_NA
-select_if(function(f) any(is.na(f)))
+datap = ndata%>%group_by(Name)%>%summarise(t_points = sum(Points))
 
-arfard(GAP_KEY <- gs_gap_key())
+if (length(gs_ws_ls(gap)) == 1){
+  # Create a new ws. 
+  # NOT NEEDED AFTER THE FIRST
+  gap <- gap %>% 
+    gs_ws_new(ws_title = "paid", input = datap,
+              trim = TRUE, verbose = FALSE)
+} else {# Adding the total number of point for each child.
+  datap$paid_on = Sys.Date()
+  gs_add_row(gap, ws = 'paid', input = datap, verbose = TRUE)
+}
 
-third_party_gap1 <- GAP_KEY %>%
-  gs_key()
-c
-gs_ws_ls(gap)
-
-f = gap %>% gs_read(ws = "Form Responses 1")
-gap %>% gs_edit_cells(ws = "Form Responses 1", input = 'Y')
-gs_edit_cells(gap, ws = "Form Responses 1", input = "SAdasd", anchor = "D2", byrow = FALSE,
-               col_names = FALSE, trim = FALSE, verbose = TRUE)
-gap
-for (i in 3:6) {
-  foo <- gap %>% gs_edit_cells(ws = "Form Responses 1", input = 'N')
-  Sys.sleep(0.3)
+# Adding the PAID flag to items paid today
+for (i in (total_rows - n_rows + 1) : (total_rows+1)) {
+  anchor_range = paste("R", i,"C5", sep = "", collapse = NULL)
+  gap %>% gs_edit_cells(ws = "Form Responses 1", input = Sys.Date(), anchor = anchor_range)
+  Sys.sleep(0.1)
 }
