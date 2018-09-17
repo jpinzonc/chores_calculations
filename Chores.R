1efb57a24af125d76cf19a254f4ee622a494e678
jpinzonc <jpinzon@hotmail.com>
2018-09-17 07:25:01 -0500

    CODE: Testing plots for data analysis

--- Chores.R
+++ Chores.R
@@ -15,7 +15,7 @@ gs_ws_ls(gap)
 data = gap %>% gs_read(ws = "Form Responses 1")
 total_rows = nrow(data)
 # generating a date field.
-data$Day = as.Date(data$Timestamp,"%m/%d/%Y")
+#data$Day = as.Date(data$Timestamp,"%m/%d/%Y")
 # NON PAID DATA
 ndata = data%>%filter(is.na(paid))
 n_rows = nrow(ndata)
@@ -42,3 +42,48 @@ for (i in (total_rows - n_rows + 1) : (total_rows+1)) {
   gap %>% gs_edit_cells(ws = "Form Responses 1", input = Sys.Date(), anchor = anchor_range)
   Sys.sleep(0.1)
 }
+
+
+eu_data = data %>% filter(Name == 'Eusebio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
+an_data = data %>% filter(Name == 'Antonio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
+
+library(ggplot2)
+
+bp= ggplot(eu_data, aes(x="", y=t_points, fill=Chores)) + geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0)
+bp + scale_fill_brewer(palette="Blues")+
+  theme_minimal()
+
+
+library(scales)
+blank_theme <- theme_minimal()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_text(size=14, face="bold")
+  )
+
+bp + scale_fill_brewer("Blues") +   blank_theme +
+  theme(axis.text.x=element_blank()) +
+  geom_text(aes(y = t_points/ nrow(eu_data) + c(0, cumsum(t_points)[-length(t_points)]), 
+                label = (t_points)), size=5
+            )
+
+
+library(plotrix)
+
+pie3D(eu_data$t_points, labels = eu_data$Chores, main = "Points per chore", 
+      explode = 0.2, radius= 0.9, labelcex = 1,  start= 0 )
+
+
+library(paletteer)
+library(waffle)
+library(cartography)
+colors =  paletteer_dynamic(cartography, blue.pal, 15)
+iron(
+  waffle(eu_data, title = "Eusebios", rows = 5, size = 1, legend_pos = "bottom", color = colors),
+  waffle(an_data, title = "Antonios", rows = 5, size = 1, legend_pos = "bottom", color = colors)
+)
--- chores_app.R
+++ chores_app.R
@@ -3,49 +3,28 @@ library(googlesheets)
 library(dplyr)
 library(DT)
 
-ui = fluidPage(sidebarLayout(
-  sidebarPanel("KID's HOME CHORES",
+ui = fluidPage(
+  titlePanel("My Application"),
+  sidebarLayout(
+    sidebarPanel(
                htmlOutput("googlechoreForm"),
                actionButton("refresh", "Refresh Goggle sheet"), width = 4),
-  mainPanel(    
-    tabsetPanel(
-      tabPanel("Historical Data", DT::dataTableOutput("dt_sheet"), id = 'dt_sheet_table'),
-      tabPanel("Not Paid", 
-               DT::dataTableOutput("not_paid_sheet"),
-               DT::dataTableOutput("owned"),
-               actionButton("Pay", "Pay"), width = 4, 
-               id = 'dt_sheet_table2')
+    mainPanel(
+      tabsetPanel(
+        tabPanel("PAYMENTS", 
+                 DT::dataTableOutput("not_paid_sheet"),
+                 DT::dataTableOutput("owned"),
+                 actionButton("payments", label = "EXECUTE PAYMENTS"),
+                 #width = 4,
+                 id = 'dt_sheet_table2'),
+        tabPanel("Historical Data", DT::dataTableOutput("dt_sheet"), id = 'dt_sheet_table')
+        )
+      )
     )
-  )
-)
 )
 
 server = function(input, output) {
-  # Get the spreadsheet from google
-  sheet = function(){
-    input$refresh
-    sheet = gs_title("NEW")
-  }
-  # Get the chores data sheet - All of it
-  historic = function(){
-    historic  = sheet() %>% gs_read(ws = "Form Responses 1")
-  }
- 
-  
- 
-   
-  not_paid = function(){
-    not_paid = historic()
-    not_paid = not_paid %>% filter(is.na(paid)) %>% select_('Timestamp', 'Name', 'Chores', 'Points')
-  }
-  
-  
-  
-  
-  
-  owned = function(){
-    datap = not_paid() %>% group_by(Name) %>% summarise(Owned = sum(Points))
-  }
+  # Renders the form. it can be filled here too. 
   output$googlechoreForm <- renderUI({
     tags$iframe(id           = "choreForm",
                 src          = "https://docs.google.com/forms/d/e/1FAIpQLSeo4BsB4IqBYP-FrqKCCK_Yu0OsOXY5kbRxVZbl84ehdL3rjw/viewform?embedded=true",
@@ -54,6 +33,24 @@ server = function(input, output) {
                 frameborder  =   0,
                 marginheight =  10)
   })
+  # Get the gsheet
+  sheet = function(){
+    input$payments
+    input$refresh
+    gs_title("NEW")
+  }
+  # Get the chores data sheet - All of it
+  historic = function(){
+    sheet() %>% gs_read(ws = "Form Responses 1")
+  }
+  # Filters the DT to show only the values that have not been paid
+  not_paid = function(){
+    historic() %>% filter(is.na(paid)) %>% select_('Timestamp', 'Name', 'Chores', 'Points')
+  }
+  # Sums the values that have not been paid
+  owned = function(){
+    not_paid() %>% group_by(Name) %>% summarise(Owned = sum(Points))
+  }
   output$dt_sheet = DT::renderDataTable({
     DT::datatable(historic(),  options = list(pageLength = 15, dom = 'tip'), rownames = FALSE)
   })
@@ -62,7 +59,16 @@ server = function(input, output) {
   })
   output$owned = DT::renderDataTable({
     DT::datatable(owned(),  options = list(pageLength = 2, dom = 't'), rownames = FALSE)
-  })  
+  })
+  observeEvent(input$payments, {
+    total_rows = nrow(historic())
+    ndata = historic() %>% filter(is.na(paid))
+    n_rows = nrow(ndata)
+    if (total_rows == n_rows){n_rows = n_rows+1}
+    for (i in (total_rows - n_rows + 2) : (total_rows+1)) {
+       anchor_range = paste("R", i, "C5", sep = "", collapse = NULL)
+       sheet() %>% gs_edit_cells(ws = "Form Responses 1", input = Sys.Date(), anchor = anchor_range)
+  }
+    })
 }
-
 shinyApp(ui = ui, server = server)
