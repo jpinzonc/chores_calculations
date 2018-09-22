b96c3dd0dee7def3ec593fb138a46e8a8c4f8e27
jpinzonc <jpinzon@hotmail.com>
2018-09-21 21:52:14 -0500

    CODE ordered the functions within the app

--- chores_app.R
+++ chores_app.R
@@ -38,79 +38,81 @@ ui = fluidPage(
 server = function(input, output) {
   # Renders the google form (It can be filled there too). 
   output$googlechoreForm <- renderUI({
-    tags$iframe(id           = "choreForm",
+     tags$iframe(id           = "choreForm",
                 src          = "https://docs.google.com/forms/d/e/1FAIpQLSeo4BsB4IqBYP-FrqKCCK_Yu0OsOXY5kbRxVZbl84ehdL3rjw/viewform?embedded=true",
                 width        = 350,
                 height       = 600,
                 frameborder  =   0,
                 marginheight =  10)
   })
+  change <- reactive({
+     paste(input$refresh , input$payments)
+  })
+  
   # Generating the data for the app
-  data = reactive({
-    input$payments
-    input$refresh
-    sheet = googlesheets::gs_title("NEW")
-    historic = sheet %>% googlesheets::gs_read(ws = "Form Responses 1")
-    not_paid =  historic %>% filter(is.na(paid)) %>% select_('Timestamp', 'Name', 'Chores', 'Points')
-    owned    = not_paid %>% group_by(Name) %>% summarise(Owned = sum(Points)) 
+  data = eventReactive(change(),{
+     sheet    = googlesheets::gs_title("NEW")
+     historic = sheet %>% googlesheets::gs_read(ws = "Form Responses 1")
+     ndata    = historic %>% filter(is.na(paid))
 
-    pals = RColorBrewer::brewer.pal(nrow(table(historic$Chores)), 'Paired')
-    paleta = setNames(pals, unique(historic$Chores))
-    
-    eu_data = historic %>% filter(Name == 'Eusebio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
-    an_data = historic %>% filter(Name == 'Antonio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
-    
-    return(list(sheet = sheet, historic = historic, not_paid = not_paid, owned = owned,
-                eu_data = eu_data, an_data = an_data, pale = paleta))})
+     total_rows = nrow(historic) 
+     n_rows = nrow(historic %>% filter(is.na(paid)==FALSE))
+
+     not_paid = ndata %>% select_('Timestamp', 'Name', 'Chores', 'Points')
+     owned    = not_paid %>% group_by(Name) %>% summarise(Owned = sum(Points)) 
+     
+     pals = RColorBrewer::brewer.pal(nrow(table(historic$Chores)), 'Paired')
+     paleta = setNames(pals, unique(historic$Chores))
+
+     eu_data = historic %>% filter(Name == 'Eusebio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
+     an_data = historic %>% filter(Name == 'Antonio') %>% select_('Chores', 'Points')%>%group_by(Chores)%>%summarise(t_points = n())
+
+     return(list(sheet = sheet, historic = historic, 
+                 not_paid = not_paid, owned = owned,
+                 eu_data = eu_data, an_data = an_data, 
+                 pale = paleta, total_rows = total_rows, n_rows = n_rows))
+     })
   # Generating the tables
   output$dt_sheet = DT::renderDataTable({
-    DT::datatable(data()$historic,  options = list(pageLength = 15, dom = 'tip'), rownames = FALSE)
+     DT::datatable(data()$historic, options = list(pageLength = 15, dom = 'tip'), rownames = FALSE)
   })
   output$not_paid_sheet = DT::renderDataTable({
-    DT::datatable(data()$not_paid,  options = list(pageLength = 10, dom = 'tip'), rownames = FALSE)
+     DT::datatable(data()$not_paid,  options = list(pageLength = 10, dom = 'tip'), rownames = FALSE)
   })
   output$owned = DT::renderDataTable({
-    DT::datatable(data()$owned,  options = list(pageLength = 2, dom = 't'), rownames = FALSE)
+     DT::datatable(data()$owned,  options = list(pageLength = 2, dom = 't'), rownames = FALSE)
   })
   observeEvent(input$payments, {
-    data = data()$historic
-    total_rows = nrow(data)
-    print(total_rows)
-    ndata = data %>% filter(is.na(paid))
-    
-    n_rows = nrow(ndata)
-    print(n_rows)
-    if (n_rows == 0)
-      {print('no updates')}
-    else{
-      for (i in (total_rows - n_rows) : (total_rows+1)) {
+     if (data()$total_rows == data()$n_rows) {cat('no updates')}
+     else{
+       for (i in (data()$n_rows+2): (data()$total_rows+1)) {
          anchor_range = paste("R", i, "C5", sep = "", collapse = NULL)
          data()$sheet %>% googlesheets::gs_edit_cells(ws = "Form Responses 1", input = Sys.Date(), anchor = anchor_range)
     }
   }
   })
   output$sumplot = renderPlot({
-    plotrix::pie3D(data()$eu_data$t_points, 
-          labels = data()$eu_data$Chores, 
-          main = "Eusebio",
-          theta = pi/4, 
+     plotrix::pie3D(data()$eu_data$t_points, 
+          labels  = data()$eu_data$Chores, 
+          main    = "Eusebio",
+          theta   = pi/4, 
           explode = 0.2, 
-          radius= 1, 
+          radius  = 1, 
           labelcex = 1,
           col = data()$pale,
-          start= 0 
+          start   = 0 
           )
   })
   output$sumplot1 = renderPlot({
-    plotrix::pie3D(data()$an_data$t_points, 
-          labels = data()$an_data$Chores, 
-          main = "Antonio",
-          theta = pi/4, 
+     plotrix::pie3D(data()$an_data$t_points, 
+          labels  = data()$an_data$Chores, 
+          main    = "Antonio",
+          theta   = pi/4, 
           explode = 0.2, 
-          radius= 1, 
+          radius  = 1, 
           labelcex = 1,  
-          col = data()$pale,
-          start= 0 
+          col     = data()$pale,
+          start   = 0 
           )
   })
 }
